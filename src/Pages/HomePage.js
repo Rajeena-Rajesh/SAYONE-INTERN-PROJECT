cb3fc14bf6bcd19efbbd316e3d4b0f1a58bcd3e3
Rajeena-Rajesh <rajeenarajesh1@gmail.com>
2022-09-28 15:29:59 +0530

    data retrieved from firebase

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,13 +1,55 @@
-import React from 'react'
-import Layout from '../Components/Layout'
+import React, { useEffect, useState } from "react";
+import Layout from "../Components/Layout";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+import fireDB from "../fireConfig";
+//import { fireproducts } from "../firecommerce-products";
 
 function HomePage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getdata();
+  }, []);
+
+  async function getdata() {
+    try {
+      const users = await getDocs(collection(fireDB, "products"));
+      const productsArray = [];
+
+      users.forEach((doc) => {
+        const obj = {
+          id: doc.id,
+          ...doc.data(),
+        };
+
+        productsArray.push(obj);
+      });
+
+      setProducts(productsArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  //  function addproductsdata(){
+  //     fireproducts.map(async (product)=>{
+  //         try {
+
+  //             await addDoc(collection(fireDB, "products"),product);
+  //         }
+
+  //        catch (error) {
+  //         console.log(error)
+  //       }
+  //     })
+
+  // }
+
   return (
     <Layout>
-      <h1>Home</h1>
-
+      <button onClick={getdata}>get data from firestore</button>
     </Layout>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
