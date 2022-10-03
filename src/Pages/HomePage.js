d2180ebd42dfeebae5bce70d244c3945907f647e
Rajeena-Rajesh <rajeenarajesh1@gmail.com>
2022-10-03 11:34:00 +0530

    search,category filter added

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Layout from "../Components/Layout";
-import { collection, addDoc, getDocs } from "firebase/firestore";
+import { collection,getDocs } from "firebase/firestore";
 import fireDB from "../fireConfig";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -8,10 +8,14 @@ import { useDispatch, useSelector } from "react-redux";
 //import { fireproducts } from "../firecommerce-products";
 function HomePage() {
   const [products, setProducts] = useState([]);
-  const[loading,setLoading]=useState(false)
-  const navigate=useNavigate()
-const {cartItems}=useSelector((state)=>state.cartReducer);
-  const dispatch=useDispatch();
+  const [loading, setLoading] = useState(false);
+  const[searchKey,setSearchKey]=useState('')
+  const[filterType,setFilterType]=useState('') 
+
+
+  const navigate = useNavigate();
+  const { cartItems } = useSelector((state) => state.cartReducer);
+  const dispatch = useDispatch();
   useEffect(() => {
     getdata();
   }, []);
@@ -22,7 +26,7 @@ const {cartItems}=useSelector((state)=>state.cartReducer);
       //   console.log(response.data);
       //   setProducts(response.data)
 
-      setLoading(true)
+      setLoading(true);
       const users = await getDocs(collection(fireDB, "products"));
       const productsArray = [];
       users.forEach((doc) => {
@@ -35,10 +39,10 @@ const {cartItems}=useSelector((state)=>state.cartReducer);
       });
 
       setProducts(productsArray);
-      setLoading(false)
+      setLoading(false);
     } catch (error) {
       console.log(error);
-      setLoading(false)
+      setLoading(false);
     }
   }
 
@@ -56,49 +60,83 @@ const {cartItems}=useSelector((state)=>state.cartReducer);
 
   // }
 
-useEffect(()=>{
-  localStorage.setItem("cartItems",JSON.stringify(cartItems))
-},[cartItems]);
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
 
-  const addToCart=(product)=>{
-    dispatch({type:"ADD_TO_CART",payload:product})
-  }
+  const addToCart = (product) => {
+    dispatch({ type: "ADD_TO_CART", payload: product });
+  };
   return (
     <Layout loading={loading}>
       <div className="container">
-        <div className="banner"><img src="../assets/slider-bg.jpg" alt="" /></div>
-        <div className="row">
-          {products.map((product) => {
-            return <div className="col-md-4">
-                <div className="m-2 p-1 product position-relative">
-                 
-                  <div className="product-content">
-                  <p>{product.name}</p>
-                  <div className="text-center">
-                    <img
-                      src={product.imageURL}
-                      alt=""
-                      className="product-img"
-                    />
-                  </div>
-                  
-                 
-                </div>
-             
-                <div className="product-actions">
-                   <h2>{product.price} RS/-</h2>
-              <div className="d-flex">
-          <button className="mx-2" onClick={()=>
-            addToCart(product)
-          }>ADD TO CART</button>
-          <button onClick={()=>{
-            navigate(`/productInfo/${product.id}`)
-          }}>VIEW</button>
-        </div>
-               </div>
+   <div className="d-flex w-50 align-items-center my-3 justify-content-center">
+   
+    <input type="text"
+    value={searchKey} 
+    className="form-control mx-2"
+    placeholder="Search Items"
+    onChange={(e)=>{
+      setSearchKey(e.target.value)
+    }}
 
+   
+    />
+     <select 
+     value={filterType}
+     className="form-control mt-3" 
+     onChange={(e)=>{
+      setFilterType(e.target.value)
+     }}>
+  <option value="">All</option>
+  <option value="electronics">Electronics</option>
+  <option value="mobile">Mobile</option>
+  <option value="fashion">Fashion</option>
+     </select>
+   </div>
+   
+
+        
+        <div className="row">
+          {products
+          .filter((obj)=>obj.name.toLowerCase().includes(searchKey))
+          .filter((obj)=>obj.category.toLowerCase().includes(filterType))
+          .map((product) => {
+            return (
+              <div className="col-md-4">
+                <div className="m-2 p-1 product position-relative">
+                  <div className="product-content">
+                    <p>{product.name}</p>
+                    <div className="text-center">
+                      <img
+                        src={product.imageURL}
+                        alt=""
+                        className="product-img"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="product-actions">
+                    <h2>{product.price} RS/-</h2>
+                    <div className="d-flex">
+                      <button
+                        className="mx-2"
+                        onClick={() => addToCart(product)}
+                      >
+                        ADD TO CART
+                      </button>
+                      <button
+                        onClick={() => {
+                          navigate(`/productInfo/${product.id}`);
+                        }}
+                      >
+                        VIEW
+                      </button>
+                    </div>
+                  </div>
+                </div>
               </div>
-              </div>
+            );
           })}
         </div>
       </div>
