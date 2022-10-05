823db56b3b81f9f8b5bf68c262769fca8cb03875
Rajeena-Rajesh <rajeenarajesh1@gmail.com>
2022-10-05 15:55:44 +0530

    Login and Registration done

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,108 +1,120 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-// import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
-// //import Loader from "../Components/Loader";
-// //import { toast } from "react-toastify";
-// import { Formik, Field, Form } from "formik";
-// import * as yup from "yup";
-// import KErrorMessage from "../Components/KErrorMessage";
+import React from "react";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {Link} from 'react-router-dom'
 
-// const validationSchema = yup.object({
+import { useFormik } from "formik"; //form handling
+import { signUpSchema } from "../schemas";
+
+const initialValues = {
+  email: "",
+  password: ""
   
-//   email: yup
-//     .string()
-//     .matches(
-//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-//       "Invalid Email"
-//     )
-//     .required("Email is Required!"),
-//   password: yup
-//     .string()
-//     .matches(
-//       /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
-//       "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character"
-//     )
-//     .required("Password is required!"),
-// });
+};
 
-// function LoginPage() {
-//   const [email, setEmail] = useState("");
-//   const [password, setPassword] = useState("");
-//   //const [loading, setLoading] = useState(false);
-//   const auth = getAuth();
+const LoginPage = () => {
+  const { values, errors, touched, handleBlurr, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: initialValues,
+      validationSchema: signUpSchema,
 
-//   const login = async () => {
-//     try {
-//       setLoading(true);
-//       const result = await signInWithEmailAndPassword(auth, email, password);
-//       localStorage.setItem("currentUser", JSON.stringify(result));
-//       setLoading(false);
-//       //toast.success("Registration Successfull");
-//       window.location.href = "/";
-//     } catch (error) {
-//       console.log(error);
-//       //toast.error("Registration Failed");
-//       setLoading(false);
-//     }
-//   };
+      onSubmit: (values, action) => {
+        console.log("values", values);
+        action.resetForm();
+      },
+    });
 
-//   return (
-//     <div className="login-parent">
-//       {/* {loading && <Loader />} */}
-//       <div className="row justify-content-center">
-//         <div className="col-md-4 z1">
+  const auth = getAuth();
+  const login = async () => {
+    try {
+      const result = signInWithEmailAndPassword(auth,
+        values.email,
+        values.password
+      );
+localStorage.setItem("currentUser",JSON.stringify(result));
+
+window.location.href="/"
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="register-parent">
+      <div className="register-top"></div>
+      <div className="row justify-content-center">
+        <div className="col-md-4 z1">
+        <form onSubmit={handleSubmit}>
+            <div className="register-form">
+              <h2>Login</h2>
+              <hr />
+
+              <input
+              className="form-control"
+                name="email"
+                type="text"
+                placeholder="email"
+                // autoComplete="off"
+                onChange={handleChange}
+                values={values.email}
+                onBlurr={handleBlurr}
+              />
+              {errors.email && touched.email ? (
+                <p className="form-error">{errors.email}</p>
+              ) : null}
+
+              <input
+               className="form-control"
+                name="password"
+                type="text"
+                //autoComplete="off"
+                placeholder="Password"
+                onChange={handleChange}
+                values={values.password}
+                onBlurr={handleBlurr}
+              />
+
+              {errors.password && touched.password ? (
+                <p className="form-error">{errors.password}</p>
+              ) : null}
+
+              {/* <input
+               className="form-control"
+                name="confirmpassword"
+                type="text"
+                autoComplete="off"
+                placeholder="Confirm Password"
+                onChange={handleChange}
+                values={values.confirmpassword}
+                onBlurr={handleBlurr}
+              />
+
+              {errors.confirmpassword && touched.confirmpassword ? (
+                <p className="form-error">{errors.confirmpassword}</p>
+              ) : null} */}
+
+              <button className="my-3" onClick={login} type="submit">
+               Login
+              </button>
+
+              <hr />
+              <Link to='/RegisterPage'>Not registered, Click here to Register!</Link>
+            </div>
+          </form>
           
+        </div>
 
-//           <Formik
-//             validationSchema={validationSchema}
-//             initialValues={{  email: "",password: "",password:"" }}
-//             onSubmit={(values) => {
-//               console.log(values);
-//             }}>
-//             <Form>
-//             <div className="login-form">
-//             <h2>Login</h2>
-//             <hr />
-//             <Field
-//               type="email"
-//               value={email}
-//               placeholder="email"
-//               className="form-control"
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//              <KErrorMessage name="email" />
-//             <Field
-//               type="password"
-//               value={password}
-//               placeholder="password"
-//               className="form-control"
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//             <KErrorMessage name="password" />
+        <div className="col-md-5">
+        <lottie-player
+            src="https://assets7.lottiefiles.com/packages/lf20_5tkzkblw.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-//             <button className="my-3" onClick={login} type="submit">
-//               LOGIN
-//             </button>
-//             <hr />
-//             <Link to="/RegisterPage">Click here to register</Link>
-//           </div>
-//             </Form>
-//           </Formik>
-//         </div>
-//         <div className="col-md-5 z1">
-//           <lottie-player
-//             src="https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json"
-//             background="transparent"
-//             speed="1"
-//             loop
-//             autoplay
-//           ></lottie-player>
-//         </div>
-//       </div>
-
-//       <div className="login-bottom"></div>
-//     </div>
-//   );
-// }
-
-// export default LoginPage;
+export default LoginPage;
--- src/Pages/RegisterPage.js
+++ src/Pages/RegisterPage.js
@@ -1,80 +1,117 @@
-// import React, { useState } from 'react'
+import React from "react";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {Link} from 'react-router-dom'
 
-// function RegisterPage() {
+import { useFormik } from "formik"; //form handling
+import { signUpSchema } from "../schemas";
 
-//     const intialValues = { email: "", password: "" };
-//   const [formValues, setFormValues] = useState(intialValues);
-//   const [formErrors, setFormErrors] = useState({});
-//   const [isSubmitting, setIsSubmitting] = useState(false);
+const initialValues = {
+  email: "",
+  password: "",
+  confirmpassword: "",
+};
 
-  
-//   return (
-//     <div>
-//       <Formik
-//        validationSchema={validationSchema}
-//        initialValues={{  email: "",password: ""}}
-//        onSubmit={(values) => {
-//          console.log(values);
-//        }}>
-//         <Form>
-//         <div className="register-parent">
-//       {loading && <Loader />}
-//       <div className="register-top"></div>
-//       <div className="row justify-content-center">
-//         <div className="col-md-5 ">
-//           <lottie-player
-//             src="https://assets7.lottiefiles.com/packages/lf20_5tkzkblw.json"
-//             background="transparent"
-//             speed="1"
-//             loop
-//             autoplay
-//           ></lottie-player>
-//         </div>
+const RegisterPage = () => {
+  const { values, errors, touched, handleBlurr, handleChange, handleSubmit } =
+    useFormik({
+      initialValues: initialValues,
+      validationSchema: signUpSchema,
 
-//         <div className="col-md-4 z1">
-//           <div className="register-form">
-//             <h2>REGISTER</h2>
-//             <hr />
+      onSubmit: (values, action) => {
+        console.log("values", values);
+        action.resetForm();
+      },
+    });
 
-//             <Field
-//               type="text"
-//               value={email}
-//               placeholder="email"
-//               className="form-control"
-//               onChange={(e) => setEmail(e.target.value)}
-//             />
-//              <KErrorMessage name="email" />
-//             <Field
-//               type="text"
-//               value={password}
-//               placeholder="password"
-//               className="form-control"
-//               onChange={(e) => setPassword(e.target.value)}
-//             />
-//             <KErrorMessage name="password" />
-//             <Field
-//               type="text"
-//               value={cpassword}
-//               placeholder="confirm password"
-//               className="form-control"
-//               onChange={(e) => setCPassword(e.target.value)}
-//             />
-//              <KErrorMessage name="password" />
-//             <button className="my-3" onClick={register} type="submit">
-//               REGISTER
-//             </button>
-//             <hr />
-//             <Link to="/LoginPage">Click here to login</Link>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//         </Form>
-//       </Formik>
-//     </div>
-//   )
-// }
+  const auth = getAuth();
+  const register = async () => {
+    try {
+      const result = await createUserWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-// export default RegisterPage
+  return (
+    <div className="register-parent">
+      <div className="register-top"></div>
+      <div className="row justify-content-center">
+        <div className="col-md-5">
+          <lottie-player
+            src="https://assets7.lottiefiles.com/packages/lf20_5tkzkblw.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player>
+        </div>
 
+        <div className="col-md-4 z1">
+          <form onSubmit={handleSubmit}>
+            <div className="register-form">
+              <h2>Register</h2>
+              <hr />
 
+              <input
+              className="form-control"
+                name="email"
+                type="text"
+                placeholder="email"
+                autoComplete="off"
+                onChange={handleChange}
+                values={values.email}
+                onBlurr={handleBlurr}
+              />
+              {errors.email && touched.email ? (
+                <p className="form-error">{errors.email}</p>
+              ) : null}
+
+              <input
+               className="form-control"
+                name="password"
+                type="text"
+                autoComplete="off"
+                placeholder="Password"
+                onChange={handleChange}
+                values={values.password}
+                onBlurr={handleBlurr}
+              />
+
+              {errors.password && touched.password ? (
+                <p className="form-error">{errors.password}</p>
+              ) : null}
+
+              <input
+               className="form-control"
+                name="confirmpassword"
+                type="text"
+                autoComplete="off"
+                placeholder="Confirm Password"
+                onChange={handleChange}
+                values={values.confirmpassword}
+                onBlurr={handleBlurr}
+              />
+
+              {errors.confirmpassword && touched.confirmpassword ? (
+                <p className="form-error">{errors.confirmpassword}</p>
+              ) : null}
+
+              <button className="my-3" onClick={register} type="submit">
+                Submit
+              </button>
+
+              <hr />
+              <Link to='/Login'>Already registered, Click here to Login!</Link>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
